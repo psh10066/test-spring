f349890c3ad85c0b40d5b7dc5c5cf36ad783204c
이호준 <psh10066@gmail.com>
2024-11-03 00:08:04 +0900

    프로젝트 생성

---
+++ src/test/java/com/psh10066/testspring/TestSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psh10066.testspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
